19b25f660b8e961eba9037f7c0925969b9a134dc
Robert Clendenin <clendenin@utexas.edu>
2019-03-12 23:51:37 -0500

    instrument selection

--- F.java
+++ F.java
@@ -1,4 +1,4 @@
-// Java program showing the implementation of a simple record
+// Java program showing how to change the instrument type
 import javax.sound.midi.*;
 import java.util.*;
 
@@ -7,59 +7,41 @@ public class F {
     public static void main(String[] args)
     {
 
-        System.out.println("Enter the number of notes to be played: ");
-        Scanner in = new Scanner(System.in);
-        int numOfNotes = in.nextInt();
+        F player = new F(); 
 
-        F player = new F();
-        player.setUpPlayer(numOfNotes);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the instrument to be played");
+        int instrument = in.nextInt();
+        System.out.println("Enter the note to be played");
+        int note = in.nextInt();
+
+        player.setUpPlayer(instrument, note);
     }
 
-    public void setUpPlayer(int numOfNotes)
+    public void setUpPlayer(int instrument, int note)
     {
 
         try {
 
-            // A static method of MidiSystem that returns
-            // a sequencer instance.
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
-
-            // Creating a sequence.
             Sequence sequence = new Sequence(Sequence.PPQ, 4);
-
-            // PPQ(Pulse per ticks) is used to specify timing
-            // type and 4 is the timing resolution.
-
-            // Creating a track on our sequence upon which
-            // MIDI events would be placed
             Track track = sequence.createTrack();
 
+            // Set the instrument type
+            track.add(makeEvent(192, 1, instrument, 0, 1));
 
-                // Adding some events to the track
-                for (int i = 5; i < (4 * numOfNotes) + 5; i += 4)
-            {
+            // Add a note on event with specified note
+            track.add(makeEvent(144, 1, note, 100, 1));
 
-                // Add Note On event
-                track.add(makeEvent(144, 1, i, 100, i));
+            // Add a note off event with specified note
+            track.add(makeEvent(128, 1, note, 100, 4));
 
-                // Add Note Off event
-                track.add(makeEvent(128, 1, i, 100, i + 2));
-            }
-
-            // Setting our sequence so that the sequencer can
-            // run it on synthesizer
             sequencer.setSequence(sequence);
-
-            // Specifies the beat rate in beats per minute.
-            sequencer.setTempoInBPM(220);
-
-            // Sequencer starts to play notes
             sequencer.start();
 
             while (true) {
 
-                // Exit the program when sequencer has stopped playing.
                 if (!sequencer.isRunning()) {
                     sequencer.close();
                     System.exit(1);
@@ -80,13 +62,9 @@ public class F {
 
         try {
 
-            // ShortMessage stores a note as command type, channel,
-            // instrument it has to be played on and its speed.
             ShortMessage a = new ShortMessage();
             a.setMessage(command, channel, note, velocity);
 
-            // A midi event is comprised of a short message(representing
-            // a note) and the tick at which that note has to be played
             event = new MidiEvent(a, tick);
         }
         catch (Exception ex) {
